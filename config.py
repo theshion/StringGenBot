81fce98dc18ce0b2271008f1a7d53ca1d3740afd
Naitik Sharma <147588654+theshion@users.noreply.github.com>
2024-11-05 22:06:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("20787644"))
+API_HASH = getenv("9dada820698e8a5fdd5e6cc78fac8567")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+BOT_TOKEN = getenv("7939164806:AAFM_IcYWxPqVVbKGbdlEThnXLQ4qHS1AYA"
+MONGO_DB_URI = getenv(mongodb+srv://abc:abc@music.dlua0.mongodb.net/yukk
+i?retryWrites=true&w=majority", None)
 
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
+OWNER_ID = int(getenv("OWNER_ID", 7050270303)
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/coding")
